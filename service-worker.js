6847ae1290c6ba8011cf5601ceee6cb84f061777
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2020-11-06 23:36:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e113bebeb14143758d2acce222a4fa4.js"
+  "/precache-manifest.3589601a60aba04dee615ba3f61fccab.js"
 );
 
 self.addEventListener('message', (event) => {
